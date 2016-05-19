f14d2dae1955cda260f91c5670f543be004785fb
dengyc <dengyc2013@gmail.com>
2016-05-19 15:58:09 +0800

    updated,

--- web-server-redux/app.js
+++ web-server-redux/app.js
@@ -6,9 +6,9 @@ app.configure(function () {
     app.use(express.bodyParser());
     app.use(app.router);
     app.set('view engine', 'jade');
-    app.set('views', __dirname + '/public');
+    app.set('views', __dirname + '/dist');
     app.set('view options', { layout: false });
-    app.set('basepath', __dirname + '/public');
+    app.set('basepath', __dirname + '/dist');
 });
 
 app.configure('development', function () {

a14dafa2ba207124bffde35fcd2ee6cfa87c610e
dengyc <dengyc2013@gmail.com>
2016-05-20 09:57:54 +0800

    updated,

--- web-server-redux/src/constants/config.ts
+++ web-server-redux/src/constants/config.ts
@@ -5,5 +5,6 @@ export var server={
     port:3014    
 }
 export var keys={
-    soundcloud_key:"75710d3a1f93f1940e7b2e2f97b11196"
+    soundcloud_key:"02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea"
+    //soundcloud_key:"75710d3a1f93f1940e7b2e2f97b11196"
 }

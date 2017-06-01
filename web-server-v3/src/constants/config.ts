bf9b714b1ab6450047b408b16eaf23f955ba933c
dengyc <dengyc2013@gmail.com>
2017-06-01 11:51:02 +0800

    Update config.ts

--- web-server-v3/src/constants/config.ts
+++ web-server-v3/src/constants/config.ts
@@ -4,6 +4,6 @@ export var app = {
     i18nPath: '/i18n'
 };
 export var keys = {
-    soundcloud_key: '02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea'
+    soundcloud_key: '2t9loNQH90kzJcsFCODdigxfp325aq4z'
     // soundcloud_key:"75710d3a1f93f1940e7b2e2f97b11196"
 };
